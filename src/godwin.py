4982c6795f2dbbff4e9cedaa3cb833ba3931f7ea
Walther Maciel <walthermaciel@gmail.com>
2015-11-21 23:17:01 -0800

    First step up the tree
    Runs one step up the comment tree and finds the number of nodes that
    are top level.

--- src/godwin.py
+++ src/godwin.py
@@ -1,5 +1,5 @@
 from pyspark import SparkConf, SparkContext, SQLContext
-from pyspark.sql.types import StructType, StructField, StringType
+from pyspark.sql.types import StructType, StructField, StringType, IntegerType
 import sys, json, re
 
 
@@ -17,9 +17,17 @@ def regex_from_words(words):
     expression = '^.*(' + expression[0:-1] + ').*$'
     return expression
 
+
+def row_into_node(r):
+    body, name, parent_id = r
+    top_level = 0
+    if parent_id.startswith('t3_'):
+        top_level = 1
+    return name, parent_id, top_level
+
 def main(argv):
     # list of words to look for!
-    GODWINS_WORDS = ['hitler', 'nazi', 'nazis', 'holocaust', 'auschwitz', 'dog', 'cat']
+    GODWINS_WORDS = ['hitler', 'nazi', 'nazis', 'holocaust', 'auschwitz', 'dog']
 
     # setup inputs and outputs
     input_directory = argv[0]
@@ -37,27 +45,44 @@ def main(argv):
     loadedJson = text.map(lambda line: json.loads(line))
 
     # make the json skinnier by removing unwanted stuff
-    fullRedditJson = loadedJson.map(lambda jObj: (jObj['subreddit'], jObj['body'], jObj['name'], jObj['parent_id'])).cache()
+    fullRedditJson = loadedJson.map(lambda jObj: (jObj['body'], jObj['name'], jObj['parent_id'])).cache()
 
     # code from greg for regex to parse lines
     linere = re.compile(regex_from_words(GODWINS_WORDS))
 
     # now filter out stuff without GODWINS_WORDS "body","id", "subreddit", "parent_id" 
-    godwinJsonList = fullRedditJson.filter(lambda (subreddit, body, name, parent_id): linere.match(body.lower())).cache()
+    godwinJsonList = fullRedditJson.filter(lambda (body, name, parent_id): linere.match(body.lower()))
 
-    # Now we convert BOTH filteredJsonList AND loadedJson into sparkSQL
+    # We don't need the comment body anymore...
+    # We need to find the paths now...
+    godwin_node_rdd = godwinJsonList.map(row_into_node)
+    full_node_rdd = fullRedditJson.map(row_into_node)
+
+    # Convert full data RDD into SQL Data Frame
     subredditSchema = StructType([
-        StructField("subreddit", StringType(), True),
-        StructField("body", StringType(), True),
         StructField("name", StringType(), True),
-        StructField("parent_id", StringType(), True)
+        StructField("parent_id", StringType(), True),
+        StructField("top_level", IntegerType(), True)
     ])
-    fullRedditDF = sqlContext.createDataFrame(fullRedditJson, subredditSchema)
-    godwinRedditDF = sqlContext.createDataFrame(godwinJsonList, subredditSchema)
+    full_node_df = sqlContext.createDataFrame(full_node_rdd, subredditSchema)
 
-    # inner join both massive tables together to match up Fist level of ids........
-    firstLevelTest = godwinRedditDF.join(fullRedditDF, [godwinRedditDF.parent_id == fullRedditDF.name], 'inner')
-    firstLevelTest.show(800)
+    # Convert godwin rows RDD into SQL Data Frame
+    godwinSchema = StructType([
+        StructField("g_name", StringType(), True),
+        StructField("g_parent_id", StringType(), True),
+        StructField("g_top_level", IntegerType(), True)
+    ])
+    godwin_node_df = sqlContext.createDataFrame(godwin_node_rdd, godwinSchema)
+
+    # Join find next layer of nodes
+    joined_df = godwin_node_df.join(full_node_df, [godwin_node_df['g_parent_id'] == full_node_df['name']])
+
+    # Drop the columns of the older node
+    next_node_df = joined_df.select('name', 'parent_id', 'top_level')
+
+    # Count how many nodes are top level
+    top_counter_df = next_node_df.groupBy('top_level').count()
+    counter_row = top_counter_df.where(top_counter_df['top_level'] == 1).select(top_counter_df['count'].alias('counter')).collect()
 
 
 if __name__ == "__main__":
